b65f4e95a55913f72edbb3b45d01198148527529
harshitgaur20031995 <harshitr0025@gmail.com>
2020-12-31 15:40:22 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const app = express();
+
+//dbconnection is my DataBase Name  
+//first create your databse
+
+const port = process.env.PORT || 3030;
+mongoose.connect('mongodb://localhost:27017/dbconnection', (err) => {
+    if (!err)
+        console.log('MongoDB connection succeeded.');
+    else
+        console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+});
+
+
+app.listen(port,()=>{
+    console.log('server is connected as : '+port);
+})
